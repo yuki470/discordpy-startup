fedd46f7831499fdfc6ea032ccf26071618b9c18
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-03 17:07:13 +0900

    implement necko

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,65 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+discord_token = 'Njg0MzA5MjE2MTY0NTc3MzAz.Xl4Otg.qWADB1kHVGCvAKJ6T5onXfUIJWw' # Discordbotのアクセストークンを入力
+discord_voice_channel_id = '684310094359560239' # 特定のボイスチャンネルを指定
+youtube_url = 'https://www.youtube.com/watch?v=wfCcs0vLysk' # youtubeのURLを指定
 
+voice = None
+player = None
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+client = discord.Client()
 
+@client.event
+async def on_ready():
+    print('Botを起動しました。')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_message(message):
+    global voice, player
+    msg = message.content
+    if message.author.bot:
+        return
+    
+    if msg == '!play':
+        if message.author.voice_channel is None:
+            await client.send_message(message.channel ,'ボイスチャンネルに参加してからコマンドを打ってください。')
+            return
+        if voice == None:
+            # ボイスチャンネルIDが未指定なら
+            if discord_voice_channel_id == '':
+                voice = await client.join_voice_channel(message.author.voice_channel)
+            # ボイスチャンネルIDが指定されていたら
+            else:
+                voice = await client.join_voice_channel(client.get_channel(discord_voice_channel_id))
+        # 接続済みか確認
+        elif(voice.is_connected() == True):
+            # 再生中の場合は一度停止
+            if(player.is_playing()):
+                player.stop()
+            # ボイスチャンネルIDが未指定なら
+            if discord_voice_channel_id == '':
+                await voice.move_to(message.author.voice_channel)
+            # ボイスチャンネルIDが指定されていたら
+            else:
+                await voice.move_to(client.get_channel(discord_voice_channel_id))
+        # youtubeからダウンロードし、再生
+        player = await voice.create_ytdl_player(youtube_url)
+        player.start()
+        return
+    
+    # 再生中の音楽を停止させる
+    if msg == '!stop':
+        if(player.is_playing()):
+                player.stop()
+                return
+    
+    # botをボイスチャットから切断させる
+    if msg == '!disconnect':
+        if voice is not None:
+            await voice.disconnect()
+            voice = None
+            return
 
+client.run(discord_token)
 
-bot.run(token)
+$ python3 discordbot.py
