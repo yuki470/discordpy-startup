4feb4c56a3a2f80ba933f67119dc81a00ce681b7
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-06 00:05:03 +0900

    試作品

--- discordbot.py
+++ discordbot.py
@@ -12,4 +12,12 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+    @client.event
+async def on_message(message):
+    if message.content.startswith('/mkch'):
+        category_id = message.channel.category_id
+        category = message.guild.get_channel(category_id)
+        new_channel = await category.create_text_channel(name='new')
+        reply = f'{new_channel.mention} を作成しました'
+        await message.channel.send(reply)
 bot.run(token)
