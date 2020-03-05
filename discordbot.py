0d9cf3c4ce81be024b0d0b4335b844a13c49728c
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-05 23:24:01 +0900

    試作中

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,16 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+@client.event
+async def on_message(message):  
+    if message.content == '/members':
+        print(message.guild.members)
+    if message.content == '/roles':
+        print(message.guild.roles':   
+    if message.content == '/text_channels':
+        print(message.guild.text_channels) 
+    if message.content == '/voice_channels':
+        print(message.guild.voice_channels)
+    if message.content == '/category_channels':
+        print(message.guild.categories)
 bot.run(token)
