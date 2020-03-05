f0ed8914dd536e1a18c6eb040512aebc79570640
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-06 00:16:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,13 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
 @client.event
 async def on_message(message):
     if message.author.bot:
@@ -19,9 +12,5 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
         
-  @client.event
-async def on_message(message):
-    if client.user in message.mentions:
-        reply = f'{message.author.mention} 呼んだ？'
-        await message.channel.send(reply) 
+
 bot.run(token)
