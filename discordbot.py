d47026b820060bd7220d0de27f0333dfd82677c0
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-05 23:34:05 +0900

    試作中

--- discordbot.py
+++ discordbot.py
@@ -12,23 +12,4 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-
-    voice = await client.join_voice_channel(client.get_channel("Discord voice channel ID"))
-    if message.content == ("lecture"):
-        player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=aFI1SItR8tg')
-        player.start()
-
-    if message.content == ("bgm"):
-        player = voice.create_ffmpeg_player('bgm.mp3')
-        player.start()
-
 bot.run(token)
