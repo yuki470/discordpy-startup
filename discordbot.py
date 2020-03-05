a9a5159c936832d820014a587693750c70cfc5db
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-06 02:24:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,26 @@ import traceback
 bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_message(message)
+    # /test と打った場合のみ反応するように
+    if message.content != '/test':
+        return
+
+    # message インスタンスから guild インスタンスを取得
+    guild = message.guild 
+
+    # ユーザとBOTを区別しない場合
+    member_count = guild.member_count
+    await message.channel.send(f'メンバー数：{member_count}')
+
+    # ユーザのみ
+    user_count = sum(1 for member in guild.members if not member.bot)
+    await message.channel.send(f'ユーザ数：{user_count}')
+
+    # BOTのみ
+    bot_count = sum(1 for member in guild.memers if member.bot)
+    await message.channel.send(f'BOT数：{bot_count}')
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
