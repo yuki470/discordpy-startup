76c485bbedee6e691511c85f019f461359544fd6
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-10 17:41:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,11 +6,7 @@ bot = commands.Bot(command_prefix='p?')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+
 
 bot.remove_command('help')
 
