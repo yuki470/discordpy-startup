a12284b2f3c7fe5cb537315e49e8b7d27ce82264
yuki470 <61646467+yuki470@users.noreply.github.com>
2020-03-12 00:15:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,47 +3,6 @@ import os
 import traceback
 import random
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    """起動時に通知してくれる処理"""
-    print('ログインしました')
-    print(client.user.name)  # ボットの名前
-    print(client.user.id)  # ボットのID
-    print(discord.__version__)  # discord.pyのバージョン
-    print('------')
-
-
-@client.event
-async def on_message(message):
-    """メッセージを処理"""
-    if message.author.bot:  # ボットのメッセージをハネる
-        return
-
-    if message.content == "!眠たい":
-        # チャンネルへメッセージを送信
-        await message.channel.send(f"{message.author.mention}さん 寝ましょう")  # f文字列（フォーマット済み文字列リテラル）
-
-    elif message.content == "!投票":
-        # リアクションアイコンを付けたい
-        q = await message.channel.send("あなたは右利きですか？")
-        [await q.add_reaction(i) for i in ('⭕', '❌')]  # for文の内包表記
-
-    elif message.content == "!おみくじ":
-        # Embedを使ったメッセージ送信 と ランダムで要素を選択
-        embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
-                              color=0x2ECC69)
-        embed.set_thumbnail(url=message.author.avatar_url)
-        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
-        await message.channel.send(embed=embed)
-
-    elif message.content == "!ダイレクトメッセージ":
-        # ダイレクトメッセージ送信
-        dm = await message.author.create_dm()
-        await dm.send(f"{message.author.mention}さんにダイレクトメッセージ")
-
-
 bot = commands.Bot(command_prefix='>')
 token = os.environ['DISCORD_BOT_TOKEN']
 
